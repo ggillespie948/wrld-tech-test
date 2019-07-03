eca751b6f688236485999bf3b3920527cac3727c
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-07-03 16:12:50 +0100

    implement KD node

--- WrldTechTest/KDimensionTree/KDNode.cs
+++ WrldTechTest/KDimensionTree/KDNode.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace WrldTechTest.KDimensionTree
 {
-    class KDNode
+    public class KDNode
     {
+        public KDNode LeftNode { get; set; }
+        public KDNode RightNode { get; set; }
+        public Feature Feature { get; set; }
+        public bool IsLeaf { get; set; }
+
+        public KDNode(Feature feature)
+        {
+            Feature = feature;
+            RightNode = null;
+            LeftNode = null;
+        }
     }
 }
