65dcdcccff691c54eb00b0c923b2fa7c6c0b7c16
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-07-03 15:51:45 +0100

    implement feature.cs

--- WrldTechTest/Feature.cs
+++ WrldTechTest/Feature.cs
@@ -6,6 +6,15 @@ namespace WrldTechTest
 {
     public class Feature
     {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Name { get; set; }
 
+        public Feature(string name, int x, int y)
+        {
+            Name = name;
+            X = x;
+            Y = y;
+        }
     }
 }
