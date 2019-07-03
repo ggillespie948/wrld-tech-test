462ef366c26205a64322c8dfd9e35adce1f71b1b
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-07-03 15:49:17 +0100

    add x unit test project to sln

---
+++ WrldTechTest-Tests/WrldTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace WrldTechTest_Tests
+{
+    public class WrldTests
+    {
+        [Fact]
+        public void Generate_Features_From_File_Works_As_Expected()
+        {
+
+        }
+    }
+}
