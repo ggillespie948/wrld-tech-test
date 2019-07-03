fd31abc08f46cd8c34f0d4023a8dabbdca40c592
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-07-03 17:07:19 +0100

    Add feature helper w/ collection of mthds used in kd tree build process + tests

---
+++ WrldTechTest-Tests/FeatureHelperTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WrldTechTest;
+using Xunit;
+
+namespace WrldTechTest_Tests
+{
+    public class FeatureHelperTests
+    {
+        [Fact]
+        public void SplitFeatureList_Works_As_Expected()
+        {
+            var features = new List<Feature>();
+            features.Add(new Feature("place1", 1, 2));
+            features.Add(new Feature("place2", 2, 2));
+            features.Add(new Feature("place3", 3, 2));
+            features.Add(new Feature("place4", 4, 2));
+            features.Add(new Feature("place5", 5, 2));
+            features.Add(new Feature("place6", 6, 2));
+            features.Add(new Feature("place7", 7, 2));
+
+            var expectedList1 = new List<Feature>();
+            expectedList1.Add(new Feature("place1", 1, 2));
+            expectedList1.Add(new Feature("place2", 2, 2));
+            expectedList1.Add(new Feature("place3", 3, 2));
+
+            var expectedList2 = new List<Feature>();
+            expectedList2.Add(new Feature("place5", 5, 2));
+            expectedList2.Add(new Feature("place6", 6, 2));
+            expectedList2.Add(new Feature("place7", 7, 2));
+
+            var outcomeLists = FeatureHelper.SplitFeatures(features, 3);
+
+            for(int i= 0; i < outcomeLists[0].Count; i++)
+            {
+                Assert.Equal(outcomeLists[0][i].Name, expectedList1[i].Name);
+            }
+
+            for (int i = 0; i < outcomeLists[1].Count; i++)
+            {
+                Assert.Equal(outcomeLists[1][i].Name, expectedList2[i].Name);
+            }
+
+        }
+
+        [Fact]
+        public void GetDistanceBetweenFeatures_Works_As_Expected()
+        {
+            var feat1 = new Feature("place1", 0, 0);
+            var feat2 = new Feature("place2", 0, 5);
+
+            var distance = FeatureHelper.GetDistance(feat2, feat1);
+
+            Assert.Equal(5, distance);
+        }
+
+        [Fact]
+        public void GetMedianFeature_Works_As_Expected()
+        {
+            var features = new List<Feature>();
+            features.Add(new Feature("place1", 1, 2));
+            features.Add(new Feature("place2", 2, 2));
+            features.Add(new Feature("place3", 3, 2));
+            features.Add(new Feature("place4", 4, 2));
+            features.Add(new Feature("place5", 5, 2));
+            features.Add(new Feature("place6", 6, 2));
+            features.Add(new Feature("place7", 7, 2));
+
+            //sort by X in test but Y is also used during building process
+            features = features.OrderBy(i => i.X).ToList();
+
+            var medianFeature = FeatureHelper.GetMedianFeatureFromList(features);
+
+            Assert.Equal("place4", medianFeature.Name);
+        }
+    }
+}
---
+++ WrldTechTest/FeatureHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WrldTechTest
+{
+    public static class FeatureHelper
+    {
+        /// <summary>
+        /// Method which returns the median feature in a given list of features. Used during the
+        /// building process of Kd Trees.
+        /// </summary>
+        /// <param name="features"> List of all features </param>
+        /// <returns> The median feature in the list </returns>
+        public static Feature GetMedianFeatureFromList(List<Feature> features)
+        {
+            int middle = ((features.Count) / 2);
+            if (middle % 2 == 0)
+            {
+                int middleIndex = Convert.ToInt32(middle - .5);
+                return features[middleIndex];
+            }
+            else
+            {
+                return features[middle];
+            }
+        }
+
+        public static double GetDistance(Feature feature1, Feature feature2)
+        {
+            return Math.Sqrt(Math.Pow((feature2.X - feature1.X), 2) + Math.Pow((feature2.Y - feature1.Y), 2));
+        }
+
+        /// <summary>
+        /// Break a list of feature into 2 chunks either side of a given index
+        /// </summary>
+        public static List<List<Feature>> SplitFeatures(List<Feature> source, int chunkElementIndex)
+        {
+            List<List<Feature>> chunks = new List<List<Feature>>();
+            chunks.Add(new List<Feature>());
+            chunks.Add(new List<Feature>());
+
+            for (int i = 0; i < source.Count; i++)
+            {
+                if (i < chunkElementIndex)
+                    chunks[0].Add(source[i]);
+                else if (i > chunkElementIndex)
+                    chunks[1].Add(source[i]);
+            }
+
+            return chunks;
+        }
+    }
+}
